8ad62ecb7203cc8b834fef6dd70bf929720923ba
Sakhil Goel <sakhilgoel@gmail.com>
2024-03-26 18:40:39 -0400

    Update README and simulate data

---
+++ scripts/00-simulate_data.R
@@ -0,0 +1,21 @@
+#### Preamble ####
+# Purpose: Simulates an analysis dataset about Pokemon level and weakness against fire
+# Author: Sakhil Goel
+# Date: 26 March 2024
+# Contact: s.goel@mail.utoronto.ca
+# License: MIT
+# Pre-requisites: -
+# Any other information needed? -
+
+
+#### Workspace setup ####
+library(tidyverse)
+
+#### Simulate data ####
+data <- 
+  tibble(
+    level = rnorm(n = 1000, mean = 100, sd = 10) |> floor(),
+    weakness = sample(x = c("fire", "not fire"), size = 1000, replace = TRUE)
+  )
+
+
